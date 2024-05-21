0af1fb9de5c1beca971f7fa8e620791ad7ef0a25
Elyas Bianchi <elyasb99@gmail.com>
2024-05-21 21:59:16 +0100

    ioctl comment

--- main.c
+++ main.c
@@ -12,9 +12,12 @@ int main() {
     return -1;
   }
 
+  // this ioctl sets any read or writes to bme_fd to use the i2c slave addr 0x77
   if (ioctl(bme_fd, I2C_SLAVE, bme_dev_addr) < 0) {
     printf("can't call ioctl");
     return -1;
   }
+
   printf("this the bme fd: %d\n", bme_fd);
+  return 0;
 }
