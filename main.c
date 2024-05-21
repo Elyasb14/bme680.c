af270d1afdc7386476ea59a481785e8463d602fc
Elyas Bianchi <elyasb99@gmail.com>
2024-05-21 12:27:19 -0700

    open the i2c file

--- main.c
+++ main.c
@@ -1,5 +1,11 @@
-#include "stdio.h"
+#include <fcntl.h>
+#include <stdio.h>
+
+#define debug
+
 
 int main() {
-  printf("hello, world\n");
+  int bme_fd  = open("/dev/i2c-1", O_RDWR);
+
+  printf("this the bme fd: %d", bme_fd);
 }
